4e74fb691105d0b0b1418b5165a82149cb53ca06
bala-amypo <bala@amypo.in>
2025-12-17 04:22:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+
+public class User {
+    private long id;
+    private String fullName;
+    private String email;
+    private String role;
+    private LocalDate createdAt;
+    public User(){
+
+    }
+    public User(String fullName, String email, String role, LocalDate createdAt) {
+        this.fullName = fullName;
+        this.email = email;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+}
