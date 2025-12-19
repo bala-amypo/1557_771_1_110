c44a88eb7484bab006d0d0deb334c97c1692be95
bala-amypo <bala@amypo.in>
2025-12-19 09:22:46 +0000

    User commit

--- src/main/java/com/example/demo/service/AllocationRuleService.java
+++ src/main/java/com/example/demo/service/AllocationRuleService.java
@@ -1,8 +1,8 @@
-package com.example.demo1.service;
+package com.example.demo.service;
 
 import java.util.List;
 
-import com.example.demo1.entity.AllocationRule;
+import com.example.demo.entity.AllocationRule;
 
 public interface AllocationRuleService {
     AllocationRule createRule(AllocationRule rule);
