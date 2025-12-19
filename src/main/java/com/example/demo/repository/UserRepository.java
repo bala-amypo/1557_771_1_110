a303f510ac67f2c390a62320a183585c24b519a9
bala-amypo <bala@amypo.in>
2025-12-19 10:10:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
-    
+     boolean existsByEmail(String email);
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package com.example.demo.service.impl;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
@@ -17,13 +18,17 @@ public class UserServiceImpl implements UserService{
     }
     @Override
     public User registerUser(User user){
+        if(userRepository.existsByEmail(user.getEmail())){
+            throw new IllegalArgumentException("User mail exists");
+        }
          return userRepository.save(user);
     }
     @Override
-    public User getUser(long id){
-        Optional<User>optionalUser = userRepository.findById(id);
-        return optionalUser.orElse(null);
-    }
+   public User getUser(long id){
+    return userRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+}
+
     @Override
     public List<User> getAllUsers(){
         return userRepository.findAll();
